62a61cfc0cbcbd237b7d200d3569064038c925a9
seongminyoo <dbtjdals1771@ajou.ac.kr>
2020-08-11 23:47:26 +0900

    update

--- 5597_과제 안 내신 분/과제 안 내신 분.cpp
+++ 5597_과제 안 내신 분/과제 안 내신 분.cpp
@@ -5,14 +5,14 @@
 using namespace std;
 
 int main() {
-	vector<bool> stu(31,false);
+	vector<bool> stu(31, false);
 	int input;
 	for (int i = 1; i <= 30; ++i) {
 		cin >> input;
 		stu[input] = true;
 	}
 	int num = 1;
-	for_each(stu.begin() + 1, stu.end(), [&stu](bool i) {
+	for_each(stu.begin() + 1, stu.end(), [&](bool i) {
 		if (i != true) {
 			cout << num << '\n';
 			num += 1;
@@ -20,5 +20,6 @@ int main() {
 		else {
 			num += 1;
 		}
-	})
+	});
+	return 0;
 }
