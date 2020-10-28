3fe5f27a2d487ecb36c6d9a88d6667faa130acf8
seongminyoo <dbtjdals1771@ajou.ac.kr>
2020-10-28 16:14:00 +0900

    backup

--- 실전코테3장_왕실의 나이트/왕실의 나이트.rb
+++ 실전코테3장_왕실의 나이트/왕실의 나이트.rb
@@ -1,19 +1,10 @@
 y, x = gets.split
 x = x.to_i
 # x는 행 y가 열
-locate = [[-2,-1],
-[-1,-2],
-[1,-2],
-[2,-1],
-[2,1],
-[1,2],
-[-1,2],
-[-2,1]]
+locate = [[-2,-1], [-1,-2], [1,-2], [2,-1], [2,1], [1,2], [-1,2],[-2,1]]
 cnt = 8
 locate.map{
-    |nx,ny|
-    nx +=x
-    ny+=y.ord - 'a'.ord  # 문자니까 'a' 아스키코드를 빼서 숫자로 만듦
+    |nx,ny| nx +=x ny+=y.ord - 'a'.ord  # 문자니까 'a' 아스키코드를 빼서 숫자로 만듦
     if nx < 1 or ny < 1
         cnt -= 1
     end
@@ -22,13 +13,3 @@ locate.map{
     end
 }
 puts cnt
-
-=begin
-    if nx < 1 or ny < 1
-        cnt -= 1
-    end
-    if nx > 8 or ny > 8
-        cnt -= 1
-    end
-=end
-
